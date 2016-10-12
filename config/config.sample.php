beaaf4926883627c73dc0a9191fae40e8b513b22
Michael Karbowiak <karbowiak@gmail.com>
2016-10-12 13:40:29 +0200

    add config sample

---
+++ config/config.sample.php
@@ -0,0 +1,28 @@
+<?php
+
+$config = array();
+
+$config["bot"] = array(
+    "prefix" => "%%"
+);
+
+$config["discord"] = array(
+    "game" => "Use %%help",
+    "token" => "",
+);
+
+$config["imgur"] = array(
+    "clientID" => "",
+    "clientSecret" => ""
+);
+
+$config["wolframalpha"] = array(
+    "appID" => ""
+);
+
+$config["cleverbot"] = array(
+    "user" => "",
+    "key" => ""
+);
+
+return $config;
